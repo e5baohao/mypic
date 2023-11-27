48c179ff76b6cfc639ff952f1184f5ded30fcceb
e5baohao <152150149+e5baohao@users.noreply.github.com>
2023-11-27 17:26:26 +0800

    Add files via upload

---
+++ style/nezhajs.js
@@ -0,0 +1,9 @@
+<!--Logo和版权-->
+window.onload = function(){
+var avatar=document.querySelector(".item img")
+var footer=document.querySelector("div.is-size-7")
+footer.innerHTML="探针"
+footer.style.visibility="visible"
+avatar.src="https://fastly.jsdelivr.net/gh/baipiaoking88/picx-images-hosting@master/akemi1.64u3k1qot3o0.webp"
+avatar.style.visibility="visible"
+}
